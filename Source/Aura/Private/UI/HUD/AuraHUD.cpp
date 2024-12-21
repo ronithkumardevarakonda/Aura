edec982bfe9c5f0bd9ccc92b906e21a231d7e135
ronithkumardevarakonda <ronithkumardevarakonda@gmail.com>
2024-12-21 17:35:29 -0500

    Aura HUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,13 @@
+// Copyright Rising Dragon
+
+
+#include "UI/HUD/AuraHUD.h"
+#include "Blueprint/UserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+}
